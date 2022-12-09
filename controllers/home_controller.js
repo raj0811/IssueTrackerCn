9d415a43e152b5b70e8134591112de52f42f3704
raj0811 <rpbarmaiya@gmail.com>
2022-12-09 12:40:02 +0530

    first commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,22 @@
+const Project = require("../models/project");
+
+module.exports.home=function(req,res){
+    console.log(req.cookies);
+
+    
+    Project.findById(req.params.id,function(err,project){
+        Project.find({}).populate('user').exec(function(err,projects){
+
+            return res.render('home',{
+                title: "Homepage",
+                projects: projects
+            })
+        
+            })
+    })
+// populate user
+
+
+}
+
+  
